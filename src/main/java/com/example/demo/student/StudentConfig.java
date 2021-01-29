8788bed07361cf3709348d983cdc0142158b0de5
Roberto Valdez <robert.tito94@gmail.com>
2021-01-29 16:47:34 -0600

    upload project

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,28 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+//    @Bean
+//    CommandLineRunner commandLineRunner(StudentRepository respository) {
+//        return args -> {
+//            Student cit = new Student(
+//                    "Citlaly chavez almanza",
+//                    "citlaly@gmail.com",
+//                    LocalDate.of(2000, Month.DECEMBER, 21));
+//            Student rob = new Student(
+//                    "Rob vr",
+//                    "vr@gmail.com",
+//                    LocalDate.of(2000, Month.AUGUST, 21));
+//
+//            respository.saveAll(List.of(cit, rob));
+//        };
+//    }
+}
